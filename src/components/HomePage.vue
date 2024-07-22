5f79bcf87f78d7a1754308ba4b6033affec0189d
DrewsClues <at869@nau.edu>
2024-07-23 00:05:35 +0900

    Home added

---
+++ src/components/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>Hello!</h1>
+
+    <h2>First time publishing VUEJS, by Andrew!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  props: {
+    msg: String,
+  },
+};
+</script>
